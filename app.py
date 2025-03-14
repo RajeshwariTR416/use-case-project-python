e20fd6cb16b3ba44eeb0251623ae7deb4b694b50
RajeshwariTR416 <rajeshwari.tr.bangalore@gmail.com>
2025-03-14 13:51:19 +0000

    modified app file

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<b>Hello,</b><br><b>Project 6: Setting up a Continuous Delivery Pipeline with Git, Jenkins, Docker, and AWS ECS!!!
-hello new jenkins file uploaded
-Thank you</b>'
+    return '<b>Hello,</b><br><b>Project : Setting up a Continuous Delivery Pipeline with Git, Jenkins, Docker, and AWS ECS!!!</b><br><b>hello new jenkins file uploaded
+            </b><br><b>Thank you</b>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
